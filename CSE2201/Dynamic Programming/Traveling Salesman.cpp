15d0bef4410b37a7f6b151254b24521177b188ba
Ashrafia Noushe <ashrafianoushe779@gmail.com>
2025-11-29 22:20:24 +0600

    --

--- CSE2201/Dynamic Programming/Traveling Salesman.cpp
+++ CSE2201/Dynamic Programming/Traveling Salesman.cpp
@@ -1,36 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 const int INF = 1e9;
 
-int main() {
-    int n;
-    cin >> n;
+int n;
+vector<vector<int>> cost;
+map<pair<int, vector<bool>>, int> dp;   // DP state
 
-    vector<vector<int>> cost(n, vector<int>(n));
-    for(int i=0;i<n;i++)
-        for(int j=0;j<n;j++)
-            cin >> cost[i][j];
+int TSP(int u, vector<bool>& visited, int cnt) {
 
-    int N = 1<<n;
-    vector<vector<int>> dp(N, vector<int>(n, INF));
+    if (cnt == n) {
+        return cost[u][0];   // return to start
+    }
 
-    dp[1][0] = 0; // starting from city 0
-
-    for(int mask=1; mask<N; mask++){
-        for(int u=0; u<n; u++){
-            if(!(mask & (1<<u))) continue;
-            for(int v=0; v<n; v++){
-                if(mask & (1<<v)) continue;
-                int nextMask = mask | (1<<v);
-                dp[nextMask][v] = min(dp[nextMask][v],
-                                      dp[mask][u] + cost[u][v]);
-            }
-        }
+    if (dp.count({u, visited})) {
+        return dp[{u, visited}];
     }
 
     int ans = INF;
-    for(int i=0;i<n;i++)
-        ans = min(ans, dp[N-1][i] + cost[i][0]);
 
-    cout << "TSP Minimum Cost = " << ans;
+    for (int v = 0; v < n; v++) {
+        if (!visited[v]) {
+            visited[v] = true;
+
+            ans = min(ans, cost[u][v] + TSP(v, visited, cnt + 1));
+
+            visited[v] = false;
+        }
+    }
+
+    return dp[{u, visited}] = ans;
 }
+
+int main() {
+    cin >> n;
+
+    cost.assign(n, vector<int>(n));
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            cin >> cost[i][j];
+
+    vector<bool> visited(n, false);
+    visited[0] = true;
+
+    int answer = TSP(0, visited, 1);
+
+    cout << "TSP Minimum Cost = " << answer;
+}
+/*
+4
+0 10 15 20
+10 0 35 25
+15 35 0 30
+20 25 30 0
+TSP Minimum Cost = 80*/
